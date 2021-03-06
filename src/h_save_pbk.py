70e4827b75cd1b8561761048651139a7dfd929cf
udexon <udexon@users.noreply.github.com>
2021-03-06 14:07:44 +0800

    Add files via upload

---
+++ src/h_save_pbk.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(driver.page_source, "html.parser")
+p(soup.text)
+p(driver.current_url)
+p(t())
+p(t().split('/'))
+p(len(t()))
+f('1 -')
+f('i:')
+p('/home/hongwu/devel/2021/Phosway/DUNIIX/auth/log')
+p(s[-1]+'/'+s[-2])
+f('5 pick:')
+f('swap: wl:')
+
